cf4e134c123b92f00c63ef606fab44d4ff6c48f1
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-19 15:47:10 +0100

    crreazione email

--- js/main.js
+++ js/main.js
@@ -3,8 +3,36 @@ const app = new Vue({
     el: '#app',
 
     data:{
+        arrEmail: [],
+    },
 
-    }
+    created(){
+
+
+        this.createEmail();
+    },
+    methods: {
+
+        createEmail(){
+
+            for (let i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                
+                .then( result =>{
+        
+                    this.arrEmail.push(result.data.response);
+
+                    console.log(this.arrEmail);
+                })
+
+            }
+
+        },
+
+    },
+
+
 })
 
 // https://flynn.boolean.careers/exercises/api/random/mail
