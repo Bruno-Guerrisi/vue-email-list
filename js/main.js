0f7d4e1e3a30065be6ec024f51fc0879fc37ae8d
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-19 16:13:54 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const app = new Vue({
     data:{
 
         arrEmail: [],
-        length: 20,
+        length: 10,
     },
 
     computed: {
@@ -28,7 +28,7 @@ const app = new Vue({
             for (let i = 0; i < this.length; i++) {
 
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                
+
                 .then( result =>{
 
                     this.arrEmail.push(result.data.response);
